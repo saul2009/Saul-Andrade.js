139af3721ed74669df543d788c64a95d103d442c
saul2009 <saulandrade124@gmail.com>
2024-02-17 18:01:14 -0800

    Student Class implemented

--- Saul-Andrade.js
+++ Saul-Andrade.js
@@ -1,1 +1,32 @@
-console.log("hello World");
+class Student 
+{
+    constructor(firstName,lastName,studentID)   {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentID = studentID;
+    }
+    
+    printFullName() {
+        console.log("Welcome " + this.firstName + " " + this.lastName);
+    }
+    
+    getStudentNumberLastTwoDigits(){
+        return this.studentID.slice(-2);
+        
+    }
+
+    nameComparisonOfStudentID(){
+        let lengthOfName = this.firstName.length + this.lastName.length
+        let lastTwoOfStudentID = this.getStudentNumberLastTwoDigits();
+        let comparisonResult = (lengthOfName >= parseInt(lastTwoOfStudentID)) ? "greater or equal" : "less";
+
+        console.log("The length of " + this.firstName + " " + this.lastName +" is " + comparisonResult + " of the last two digits of student ID (" + lastTwoOfStudentID + ")");
+    }
+    
+} 
+
+let studentA = new Student('Saul','Andrade', '123456789');
+studentA.printFullName();
+let lastTwoDigits = studentA.getStudentNumberLastTwoDigits();
+console.log("Student's last two digits is " + lastTwoDigits);
+studentA.nameComparisonOfStudentID();
